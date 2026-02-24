452a1a1e981b10062ffbc07f5d7d9be834d1f85c
Sadiya Irin <kazisadiya@joinventureai.com>
2026-02-24 10:59:35 +0600

    commented entrypoint.sh and added uvicorn in requirements.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
-set -e
+    # #!/bin/bash
+    # set -e
 
-echo "Waiting for Redis..."
-until nc -z -v -w30 ${REDIS_HOST:-dokploy-redis} ${REDIS_PORT:-6379}; do
-    echo "Waiting for Redis..."
-    sleep 1
-done
+    # echo "Waiting for Redis..."
+    # until nc -z -v -w30 ${REDIS_HOST:-dokploy-redis} ${REDIS_PORT:-6379}; do
+    #     echo "Waiting for Redis..."
+    #     sleep 1
+    # done
 
-echo "Waiting for PostgreSQL..."
-until nc -z -v -w30 ${DB_HOST} ${DB_PORT}; do
-    echo "Waiting for PostgreSQL..."
-    sleep 1
-done
+    # echo "Waiting for PostgreSQL..."
+    # until nc -z -v -w30 ${DB_HOST} ${DB_PORT}; do
+    #     echo "Waiting for PostgreSQL..."
+    #     sleep 1
+    # done
 
-echo "Running migrations..."
-python3 manage.py migrate --noinput
+    # echo "Running migrations..."
+    # python3 manage.py migrate --noinput
 
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+    # echo "Collecting static files..."
+    # python3 manage.py collectstatic --noinput
 
-echo "Starting Daphne server..."
-exec daphne -b 0.0.0.0 -p 8500 src.asgi:application
+    # echo "Starting Daphne server..."
+    # exec daphne -b 0.0.0.0 -p 8500 src.asgi:application
